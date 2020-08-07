6db8c3918b7db57601cf02bf74c5e13624d833dd
alibek <alibek_ch@yahoo.com>
2020-08-07 20:11:29 +0600

    update with gitignore, nosteup

---
+++ notasetup.py
@@ -0,0 +1,20 @@
+# from setuptools import setup 
+
+# setup(
+#     name="Django to Git",#ЛЮБОЕ НАИМЕНОВАНИЕ
+#     url = "https://github.com/alibekch/lesson36",
+#     author = "alibek",
+#     packages = ["models"], #ЭТО ПАПКА КОТОРАЯ БУДЕТ ПРЕОБРАЗОВЫВАТЬСЯ
+#     installrequires=[],
+#     license="MIT",
+#     description = "my own lib"
+# )
+
+
+
+# echo "# lesson36" >> README.md
+# git init ==>создание файла с конфигурацией
+# git add README.md 
+# git commit -m "first commit" ==>комментирование изменений
+# git remote add origin https://github.com/alibekch/lesson36.git ==>соединение с репозиторием
+# git push -u origin master ==> загрузка изменений в гит
